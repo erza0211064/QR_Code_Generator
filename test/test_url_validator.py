c346568b04e3e8d5796edc414d6f99c3c12f414d
Tony, Yang <erza0211064@gmail.com>
2026-07-30 23:38:33 -0700

    fix CI, bug in image generate and test case

--- test/test_url_validator.py
+++ test/test_url_validator.py
@@ -7,13 +7,18 @@ def test_valid_url():
     assert result_url == validate_url(url)
 
 def test_blocked_domain_url():
-    url = "evil.com"
+    url = "https://evil.com/login"
     with pytest.raises(ValueError) as exc_info:
         validate_url(url)
-    assert "This URL is Blocked" in str(exc_info)
+    assert "This URL is Blocked" in str(exc_info.value)
 
 def test_too_long_url():
     url = "https://example.com" + "a"*2050
     with pytest.raises(ValueError) as exc_info:
         validate_url(url)
-    assert "URL exceed max url length" in str(exc_info)
+    assert "URL exceed max url length" in str(exc_info.value)
+
+def test_normalize_url():
+    url = "https://GOOGLE.COM"
+    result_url = "https://google.com"
+    assert result_url == validate_url(url)
